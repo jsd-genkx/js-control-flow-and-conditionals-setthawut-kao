efd5ada52e8e8bf997b0592ce5be6f526c59cf98
setthawut-kao <setthawut.kao@gmail.com>
2025-07-15 11:43:46 +0700

    task01 solution

--- task02.js
+++ task02.js
@@ -3,5 +3,12 @@
 // TODO: Use if/else to check if it's a number
 // (hint: typeof or isNaN)
 
-const userInput = "42";
+const userInput = "NaN";
 // Try changing to "hello", 10, null, NaN
+
+if (!isNaN(Number(userInput))) {
+  console.log("It's a number");
+} else {
+  console.log("Not a number");
+}
+
